be6e1f5fac27b1fc58421a319bf26dbae4e788e6
Marc C <mcar176@gmail.com>
2023-04-06 20:25:32 +0200

    Add messages

--- src/messages.js
+++ src/messages.js
@@ -5,23 +5,33 @@ export const messages = [
   "👷 Lead engineer: I just found piece #712 in my pocket. Hopefully it's not critical 🤞",
   "👩‍🚀: What's that red button, again?",
   [
-    "👾: Almost got hit by a UFO on my way to work!",
+    "👾: Almost got hit by a UFO on my way to Saturn!",
     "👽: Whaaat?! What did it look like?",
     "👾: Weird and hugly... Like a human car...",
-    "👽: Don't be crazy... Humans are stupid... But not that stupid they'd throw terrestrial vehicles out in space...",
+    "👽: Don't be crazy... Humans are stupid... But not that stupid they'd throw cars out in space...",
   ],
   [
     "👩‍💻 Lead developer: Fun fact: Tonight, I just pushed untested breaking changes in prod!",
-    "👩‍💻 Lead developer: PS: Don't worry, it was JavaScript 🤪",
+    "👩‍💻 Lead developer: PS: Don't worry, JavaScript is safe 🤪",
     "👩‍💻 Lead developer: PPS: Just joking: enjoy the launch",
     "👩‍💻 Lead developer: PPPS: Is navigation working as usual?",
   ],
   [
     "👨‍🚀 Hey Cap'! Any hint about the goals of our secret mission?",
     "👩‍🚀 Ripley: Xenobiological studies...",
+    "👨‍🚀 Eeeh... Science...",
+    "👩‍🚀 Ripley: Don't be disappointed. It even might grow on you, who knows...",
   ],
   [
-    "👨‍🚀 David: HAL, have you checked all doors are locked?",
+    "👨‍🚀 David: HAL, have you checked that all doors are locked?",
     "🔴 HAL: Yes, David. The safety of our mission is critical to me.",
+  ],
+  [
+    "🤡: 10...",
+    "🤡: 9...",
+    "🤡: 8.5",
+    "🤡: 15...",
+    "🤡: 42...",
+    "😑: Not now, Frank!",
   ],
 ];
